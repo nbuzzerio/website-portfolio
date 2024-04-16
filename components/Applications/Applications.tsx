46dc57602dcdece5116a4a63b0c9d80ca0762f86
nbuzzerio <nick.buzzerio@hotmail.com>
2024-04-16 19:58:08 -0400

    added 2 apps and improved the card styles

--- components/Applications/Applications.tsx
+++ components/Applications/Applications.tsx
@@ -4,8 +4,8 @@ function Applications(props) {
       <h2 className="py-10 text-5xl text-black underline lg:text-7xl">
         Applications
       </h2>
-      <div className="applications-wrapper grid w-8/12 grid-cols-1 justify-center gap-20 py-12 lg:flex lg:w-10/12 2xl:w-8/12">
-        <div className="app flex w-full flex-col overflow-hidden rounded-xl border border-primary-darker bg-primary-darker/80">
+      <div className="applications-wrapper grid w-8/12 grid-cols-1 justify-center gap-10 py-12 lg:flex lg:w-10/12 lg:flex-wrap 2xl:w-8/12">
+        <div className="app flex w-full basis-[calc((100%-80px)/3)] flex-col overflow-hidden rounded-xl border border-primary-darker bg-primary-darker/80">
           <div className="appThumbnail group aspect-[1.33] w-full">
             <div className="appThumbnailInner relative h-full w-full overflow-x-visible transition duration-700 group-hover:[transform:rotateY(180deg)] ">
               <img
@@ -18,11 +18,11 @@ function Applications(props) {
               ></img>
             </div>
           </div>
-          <div className="appInfo flex flex-col p-4">
+          <div className="appInfo flex grow flex-col justify-between p-4">
             <h4 className="appTitle py-3 text-lg text-white sm:text-2xl lg:text-xl xl:text-2xl">
               LTO Moonbase Website
             </h4>
-            <p className="appDescription indent-8 text-base text-white sm:text-xl lg:text-lg xl:text-xl">
+            <p className="appDescription grow indent-8 text-base text-white sm:text-xl lg:text-lg xl:text-xl">
               As the full stack developer for the cryptocurrency leasing node,
               LTO Moonbase, I created this website using React and NodeJS from
               the ground up. I worked closely with the business team to design
@@ -32,7 +32,7 @@ function Applications(props) {
               stakeholders.
             </p>
             <a
-              className="appLinks relative py-3 text-base text-white hover:font-bold sm:text-xl lg:text-lg xl:text-xl"
+              className="appLinks relative py-3 text-base text-white hover:underline sm:text-xl lg:text-lg xl:text-xl"
               rel="noreferrer"
               target="_blank"
               href="https://ltomoonbase.club/"
@@ -41,7 +41,80 @@ function Applications(props) {
             </a>
           </div>
         </div>
-        <div className="app flex w-full flex-col overflow-hidden rounded-xl border border-primary-darker bg-primary-darker/80">
+        <div className="app flex w-full basis-[calc((100%-80px)/3)] flex-col overflow-hidden rounded-xl border border-primary-darker bg-primary-darker/80">
+          <div className="appThumbnail group aspect-[1.33] w-full">
+            <div className="appThumbnailInner relative h-full w-full overflow-x-visible transition duration-700 group-hover:[transform:rotateY(180deg)] ">
+              <img
+                className="appThumbnailImg-front absolute h-full w-full"
+                src="https://nb-portfolio-images.s3.amazonaws.com/chessApp.jpeg"
+              ></img>
+              <img
+                className="appThumbnailImg-back absolute h-full w-full [transform:rotateY(180deg)]"
+                src="/images/chess-site.jpg"
+              ></img>
+            </div>
+          </div>
+          <div className="appInfo flex grow flex-col justify-between p-4 text-white">
+            <h4 className="appTitle py-3 text-lg text-white sm:text-2xl lg:text-xl xl:text-2xl">
+              Game Catalog
+            </h4>
+            <p className="appDescription grow indent-8 text-base text-white sm:text-xl lg:text-lg xl:text-xl">
+              Search through a catalog of games by name, platform, or category.
+              Filter by release date, popularity, and much more! This
+              application enables you to discover extensive information about a
+              wide variety of video games through a seamless and user-friendly
+              interface. Discover what fellow gamers and critics think about the
+              games you&apos;re interested in, view screenshots, read synopses,
+              and perhaps even watch trailers!
+            </p>
+            <a
+              className="appLinks relative py-3 text-base text-white hover:underline sm:text-xl lg:text-lg xl:text-xl"
+              rel="noreferrer"
+              target="_blank"
+              href="https://nickbuzzerio.com/games-catalog/"
+            >
+              Find a Game
+            </a>
+          </div>
+        </div>
+        <div className="app flex w-full basis-[calc((100%-80px)/3)] flex-col overflow-hidden rounded-xl border border-primary-darker bg-primary-darker/80">
+          <div className="appThumbnail group aspect-[1.33] w-full">
+            <div className="appThumbnailInner relative h-full w-full overflow-x-visible transition duration-700 group-hover:[transform:rotateY(180deg)] ">
+              <img
+                className="appThumbnailImg-front absolute h-full w-full"
+                src="https://nb-portfolio-images.s3.amazonaws.com/chessApp.jpeg"
+              ></img>
+              <img
+                className="appThumbnailImg-back absolute h-full w-full [transform:rotateY(180deg)]"
+                src="/images/chess-site.jpg"
+              ></img>
+            </div>
+          </div>
+          <div className="appInfo flex grow flex-col justify-between p-4 text-white">
+            <h4 className="appTitle py-3 text-lg text-white sm:text-2xl lg:text-xl xl:text-2xl">
+              Algorithm Showcase
+            </h4>
+            <p className="appDescription grow indent-8 text-base text-white sm:text-xl lg:text-lg xl:text-xl">
+              Small collection of software engineering interview prompts. Each
+              one is accompanied by a solution I have written to solve the
+              prompt, including a breakdown of the Time and Space complexities
+              of the solution in Big O notation. Come check out some of my
+              solutions and maybe try out some of these coding challenges
+              yourself!
+            </p>
+            <a
+              className="appLinks relative py-3 text-base text-white hover:underline sm:text-xl lg:text-lg xl:text-xl"
+              rel="noreferrer"
+              target="_blank"
+              href="https://nickbuzzerio.com/chess/"
+            >
+              Check my work!
+            </a>
+          </div>
+        </div>
+
+        {/* row 2 */}
+        <div className="app flex w-full basis-[calc((100%-80px)/3)] flex-col overflow-hidden rounded-xl border border-primary-darker bg-primary-darker/80">
           <div className="appThumbnail group aspect-[1.33] w-full">
             <div className="appThumbnailInner relative h-full w-full overflow-x-visible transition duration-700 group-hover:[transform:rotateY(180deg)] ">
               <img
@@ -57,11 +130,11 @@ function Applications(props) {
               ></img>
             </div>
           </div>
-          <div className="appInfo flex flex-col p-4 text-white">
+          <div className="appInfo flex grow flex-col justify-between p-4 text-white">
             <h4 className="appTitle py-3 text-lg text-white sm:text-2xl lg:text-xl xl:text-2xl">
               YouTube Subscription Organizer
             </h4>
-            <p className="appDescription indent-8 text-base text-white sm:text-xl lg:text-lg xl:text-xl">
+            <p className="appDescription grow indent-8 text-base text-white sm:text-xl lg:text-lg xl:text-xl">
               Have too many YouTube subs? Can&apos;t find the genre you&apos;re
               looking for or miss out on a video not placed in your feed? YTSO
               is here to solve that. This application lets you log into YouTube
@@ -69,13 +142,14 @@ function Applications(props) {
               neatly drop them into categories letting you see uploads only for
               what you are in the mood to watch all sorted by release date!
             </p>
-            {/* <a className="appLinks py-3 text-white relative hover:font-bold text-base sm:text-xl lg:text-lg xl:text-xl" href="https://nickbuzzerio.com/ytso/">
+            {/* <a className="appLinks py-3 text-white relative hover:underline text-base sm:text-xl lg:text-lg xl:text-xl" 
+            href="https://nickbuzzerio.com/ytso/">
                 Try it out
               </a> */}
           </div>
         </div>
 
-        <div className="app flex w-full flex-col overflow-hidden rounded-xl border border-primary-darker bg-primary-darker/80">
+        <div className="app flex w-full basis-[calc((100%-80px)/3)] flex-col overflow-hidden rounded-xl border border-primary-darker bg-primary-darker/80">
           <div className="appThumbnail group aspect-[1.33] w-full">
             <div className="appThumbnailInner relative h-full w-full overflow-x-visible transition duration-700 group-hover:[transform:rotateY(180deg)] ">
               <img
@@ -88,11 +162,11 @@ function Applications(props) {
               ></img>
             </div>
           </div>
-          <div className="appInfo flex flex-col p-4 text-white">
+          <div className="appInfo flex grow flex-col justify-between p-4 text-white">
             <h4 className="appTitle py-3 text-lg text-white sm:text-2xl lg:text-xl xl:text-2xl">
               Multiplayer Chess
             </h4>
-            <p className="appDescription indent-8 text-base text-white sm:text-xl lg:text-lg xl:text-xl">
+            <p className="appDescription grow indent-8 text-base text-white sm:text-xl lg:text-lg xl:text-xl">
               Want to play a quick game of Chess over Zoom with a friend without
               all the hassle? Multiplayer Chess does just that. No need to
               create an account, remember passwords, or send invite codes 10
@@ -101,7 +175,9 @@ function Applications(props) {
               moving the pieces in real time with the help of Socket.io.
             </p>
             <a
-              className="appLinks relative py-3 text-base text-white hover:font-bold sm:text-xl lg:text-lg xl:text-xl"
+              className="appLinks relative py-3 text-base text-white hover:underline sm:text-xl lg:text-lg xl:text-xl"
+              rel="noreferrer"
+              target="_blank"
               href="https://nickbuzzerio.com/chess/"
             >
               Play a Game
