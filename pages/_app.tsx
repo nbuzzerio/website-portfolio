67d904c379307398378c32712f75254c2fdfa922
nbuzzerio <nick.buzzerio@hotmail.com>
2026-07-20 13:33:42 -0400

    feat: add Cloudflare Web Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,23 @@
 import "@/styles/main.scss";
 import Layout from "@/components/Layout";
 import AuthContext from "@/components/AuthContext";
+import Script from "next/script";
 
 function App({ Component, pageProps }) {
   return (
-    <AuthContext>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AuthContext>
+    <>
+      <Script
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        strategy="afterInteractive"
+        data-cf-beacon='{"token":"81f90747808b4b718ff9e8fb83e659dd"}'
+      />
+
+      <AuthContext>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthContext>
+    </>
   );
 }
 
